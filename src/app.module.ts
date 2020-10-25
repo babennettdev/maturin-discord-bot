b02f37bd8ab60d3833cc06787c603b802a2f2028
babennettdev <babennettdev@gmail.com>
2020-10-24 23:25:53 -0400

    Lint fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { GunslingerLitanyModule } from './commands/darkTower/gunslingerLitany/gunslingerLitany.module';
 import { DiscordClientModule } from './botCore/discordClient/discordClient.module';
-import { ListenerModule } from './botCore/listener/listener.module';
-import { CommandReflectorModule } from './core/decoratorReflectors/commandReflector.module';
 import { CommandsModule } from './commands/commands.module';
 
 @Module({
